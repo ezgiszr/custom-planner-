2bb934b128ac238cd79f8072d173322a1dde7c14
Ezgi İşgüzar <ezgiisguzar10@gmail.com>
2026-04-16 16:48:40 +0300

    feat: .env file created and infos rotate here

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAm3JPi51XdnSSQ8uz6PH8ao0eOJHL6aeI",
-  authDomain: "manage-your-time-e04ae.firebaseapp.com",
-  projectId: "manage-your-time-e04ae",
-  storageBucket: "manage-your-time-e04ae.firebasestorage.app",
-  messagingSenderId: "136411655429",
-  appId: "1:136411655429:web:9ab81a07dbbfdb498bdb6c",
-  measurementId: "G-W9L2EVJ40E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
